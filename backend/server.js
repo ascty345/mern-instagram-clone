f83a7317810f86a3e3ced15f0a041e33b95fb17c
tue <tuedinh28@gmail.com>
2022-04-04 18:16:07 +0700

    set custom Middlewares for error handling

--- backend/server.js
+++ backend/server.js
@@ -2,15 +2,20 @@ import express from 'express'
 import colors from 'colors'
 import dotenv from 'dotenv'
 
+import { notFound, errorHandler } from './middlewares/errorMiddleware.js'
+
 dotenv.config()
 
 const app = express()
 
-const PORT = process.env.PORT || 5000
-
 app.get('/', (req, res) => {
   res.send('Hello World')
 })
+
+app.use(notFound)
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 5000
 
 app.listen(PORT, () => {
   PORT,
