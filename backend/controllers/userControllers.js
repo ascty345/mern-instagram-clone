4c6dcc28f632e0b81f0f1258cdcf78c0e14519ed
tue <tuedinh28@gmail.com>
2022-04-05 15:30:14 +0700

    user signin, generate token and authMiddleware

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -1,4 +1,5 @@
 import asyncHandler from 'express-async-handler'
+import generateToken from '../utils/generateToken.js'
 import User from '../models/userModel.js'
 
 // @desc   Register a new user
@@ -26,6 +27,7 @@ const registerUser = asyncHandler(async (req, res) => {
       _id: user._id,
       name: user.name,
       email: user.email,
+      token: generateToken(user._id),
     })
   } else {
     res.status(400)
@@ -33,4 +35,25 @@ const registerUser = asyncHandler(async (req, res) => {
   }
 })
 
-export { registerUser }
+// @desc   Auth user & get token
+// @route  POST /api/users/signin
+// @access Public
+const authUser = asyncHandler(async (req, res) => {
+  const { email, password } = req.body
+
+  const user = await User.findOne({ email })
+
+  if (user && (await user.matchPassword(password))) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(401)
+    throw new Error('Invalid email or password')
+  }
+})
+
+export { registerUser, authUser }
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,11 +1,8 @@
 import express from 'express'
 const router = express.Router()
-import { registerUser } from '../controllers/userControllers.js'
-
-router.get('/', (req, res) => {
-  res.send('hello')
-})
+import { registerUser, authUser } from '../controllers/userControllers.js'
 
 router.post('/signup', registerUser)
+router.post('/signin', authUser)
 
 export default router
